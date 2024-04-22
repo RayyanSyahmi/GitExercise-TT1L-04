669ea4f2e5b6dafc70fffb91aaa46fd21527f0e5
Rayyan <eansyahmi@gmail.com>
2024-04-22 19:22:11 +0800

    moved code from size selection into brushes

--- canvas.py
+++ canvas.py
@@ -1,25 +1,31 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
+from brushes import Brush, BrushSizeInput
 
 class PaintCanvas(QtWidgets.QLabel):
     def __init__(self):
         super().__init__()
 
-        self.setPixmap(QtGui.QPixmap(800, 600)) # Set the canvas size to 1200x900
+        self.setPixmap(QtGui.QPixmap(800, 500))
         self.pixmap().fill(QtCore.Qt.white)
+
         self.last_pos = None
-        self.brush_radius = 5
-        self.brush_offset = QtCore.QPoint(0, 0)
+        self.brush = Brush()
 
-        self.brush_size_input = QtWidgets.QLineEdit(self)
-        self.brush_size_input.setPlaceholderText("Brush size")
+        self.brush_size_input = BrushSizeInput(self.brush)
         self.brush_size_input.setGeometry(10, 10, 80, 20)
-        self.brush_size_input.returnPressed.connect(self.set_brush_radius_from_input)
 
-    def set_brush_radius_from_input(self):
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.brush_size_input)
+        self.setLayout(layout)
+
+        # Connect the textChanged signal to the update_brush_size method
+        self.brush_size_input.brush_size_input.textChanged.connect(self.update_brush_size)
+
+    def update_brush_size(self, text):
         try:
-            self.brush_radius = int(self.brush_size_input.text())
-            self.brush_size_input.clear()
+            self.brush.size = int(text)
+            self.brush.set_size(self.brush.size)
         except ValueError:
             pass
 
@@ -30,7 +36,8 @@ class PaintCanvas(QtWidgets.QLabel):
     def mouseMoveEvent(self, event):
         if event.buttons() & QtCore.Qt.LeftButton and self.last_pos:
             painter = QtGui.QPainter(self.pixmap())
-            painter.setPen(QtGui.QPen(QtCore.Qt.black, self.brush_radius * 2))
+            pen = QtGui.QPen(QtGui.QPen(self.brush.color, self.brush.size))
+            painter.setPen(pen)
             painter.setBrush(QtCore.Qt.black)
             painter.drawLine(self.last_pos, event.pos())
             painter.end()
@@ -38,6 +45,8 @@ class PaintCanvas(QtWidgets.QLabel):
             self.last_pos = event.pos()
 
 app = QtWidgets.QApplication(sys.argv)
+
 window = PaintCanvas()
 window.show()
+
 app.exec_()

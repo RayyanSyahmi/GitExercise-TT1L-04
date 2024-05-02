413d5882cfa8b6f48d9fbc3d65fc6c8fd7fa02e1
unknown <mveevinraj14@gmail.com>
2024-05-02 16:43:50 +0800

    hotkeys

--- save.py
+++ save.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog 
+import keyboard  # type: ignore
 
 def save_file():
     types = [ ("Text Files", "*.txt"),
@@ -7,8 +8,31 @@ def save_file():
               ("Png Files", "*.png"),
               ("Jpeg Files", "*.jpg *.jpeg")]
 
+COMBINATIONS =[
+     {keyboard.key.shift, keyboard.KeyCode(char='s') }]  [
+         {keyboard.key.shift, keyboard.Keycode(char='a') }   ]
+    
+
+current = set()
+
+def execute():
+   print("Detected hotkey")
+   
+def on_press(key):
+     if any([key in COMBO  for COMBO in COMBINATIONS]):
+       current.add(key)
+       if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+           execute()
+
+def on_release(key):
+    if any([key in COMBO for COMBO in COMBINATIONS]):
+     current.remove(key)
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:listener.join()
+
+
     file_path = QFileDialog.str(title = "Custom Save Title",
-                                             filetypes= types, initialdir=".")
+                                             filetypes= type , initialdir=".")
     data = Entry.txt()  # type: ignore
 
     if file_path != "":

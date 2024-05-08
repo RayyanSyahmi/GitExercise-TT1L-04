3a93c28a58b3d77c1e975ed59389262d0c766558
VeevinRaj <167269313+VeevinRaj@users.noreply.github.com>
2024-05-08 08:29:49 +0800

    Update save.py

--- save.py
+++ save.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog 
+import keyboard  # type: ignore
 
 def save_file():
     types = [ ("Text Files", "*.txt"),
@@ -7,12 +8,34 @@ def save_file():
               ("Png Files", "*.png"),
               ("Jpeg Files", "*.jpg *.jpeg")]
 
-    file_path = QFileDialog.asksavesfilename(title = "Custom Save Title",
-                                             filetypes= types, initialdir=".")
-    data = entry.get() # type: ignore
+COMBINATIONS =[
+     {keyboard.key.shift, keyboard.KeyCode(char='s') }]  [
+         {keyboard.key.shift, keyboard.Keycode(char='a') }   ]
+    
+
+current = set()
+
+def execute():
+   print("Detected hotkey")
+   
+def on_press(key):
+     if any([key in COMBO  for COMBO in COMBINATIONS]):
+       current.add(key)
+       if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+           execute()
+
+def on_release(key):
+    if any([key in COMBO for COMBO in COMBINATIONS]):
+     current.remove(key)
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:listener.join()
+
+
+    file_path = QFileDialog.str(title = "Custom Save Title",
+                                             filetypes= type , initialdir=".")
+    data = Entry.txt()  # type: ignore
 
     if file_path != "":
-        file_writter = open(file_path, mode = 'W')
+        file_writter = open(file_path, mode = 'w')
         file_writter.writter(data)
         file_writter.close()
-    

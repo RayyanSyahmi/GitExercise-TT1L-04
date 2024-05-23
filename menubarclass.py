9cd3d556bb800230110946376959a51cb9ee7dd9
unknown <mveevinraj14@gmail.com>
2024-05-23 18:12:21 +0800

    undo&redo

--- menubarclass.py
+++ menubarclass.py
@@ -3,14 +3,27 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from canvasclass import Canvas
 import sys
 
 class MyMenuBar(QtWidgets.QMenuBar):
-    def __init__(self):
+    def __init__(self, canvas):
         super().__init__()
+        self.canvas = canvas
 
         file_menu = self.addMenu("File")
         file_menu.addAction("New")
         file_menu.addAction("Open")
-        file_menu.addAction("Save")
-        file_menu.addAction("Exit")
+
+        saveAction = QAction("Save", self)
+        saveAction.triggered.connect(self.save)
+        self.save_action = file_menu.addAction(saveAction)
+
+    def save(self):
+        print("Save button pressed")
+        filePath, _ = QFileDialog.getSaveFileName(self, "Save Image", "",
+                        "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
+    
+        if filePath == "":
+            return
+        self.canvas.save(filePath)

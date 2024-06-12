0ea1b7456dfda1a81ed1c3333fcf8bffa8dff94f
Shaveen <shaveen7561@gmail.com>
2024-06-12 09:48:41 +0800

    merge

--- Mainwindow.py
+++ Mainwindow.py
@@ -1,21 +1,10 @@
 import sys
-import os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget, QAction
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from menubarclass import MyMenuBar
 from sidebar import Sidebar
 from canvas import Canvas
-
-def resource_path(relative_path):
-    """ Get the absolute path to a resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -47,7 +36,7 @@ class MainWindow(QMainWindow):
         else:
             self.dock_widget.show()
             self.sidebar_toggle.setText("Tools")
-
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setFont(QFont("Segoe UI", 9))
